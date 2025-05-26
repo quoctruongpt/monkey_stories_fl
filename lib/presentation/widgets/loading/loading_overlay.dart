23ad54d264095b3f0c8b66bc0ba25c81b52291d0
Quốc Trưởng KT <truong.dinh@hoc10.com>
2025-05-26 17:00:45 +0700

    feat: chuyển đổi LoadingOverlay từ StatelessWidget sang StatefulWidget và thêm hiệu ứng hoạt hình cho CircularProgressIndicator

--- lib/presentation/widgets/loading/loading_overlay.dart
+++ lib/presentation/widgets/loading/loading_overlay.dart
@@ -4,7 +4,7 @@ import 'package:monkey_stories/core/utils/lottie_utils.dart';
 
 /// A reusable loading overlay widget that displays a semi-transparent barrier
 /// and a centered CircularProgressIndicator.
-class LoadingOverlay extends StatelessWidget {
+class LoadingOverlay extends StatefulWidget {
   final Color barrierColor;
   final bool dismissible;
 
@@ -15,18 +15,62 @@ class LoadingOverlay extends StatelessWidget {
   });
 
   @override
+  State<LoadingOverlay> createState() => _LoadingOverlayState();
+}
+
+class _LoadingOverlayState extends State<LoadingOverlay>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<double>(
+      begin: 0.9,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         // ModalBarrier to dim the background and block taps
-        ModalBarrier(dismissible: dismissible, color: barrierColor),
+        ModalBarrier(
+          dismissible: widget.dismissible,
+          color: widget.barrierColor,
+        ),
         // Centered CircularProgressIndicator
         Center(
-          child: LottieBuilder.asset(
-            'assets/lottie/loading.lottie',
-            width: 50,
-            height: 50,
-            decoder: customDecoder,
+          child: ScaleTransition(
+            scale: _animation,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: LottieBuilder.asset(
+                  'assets/lottie/loading.lottie',
+                  width: 50,
+                  height: 50,
+                  decoder: customDecoder,
+                ),
+              ),
+            ),
           ),
         ),
       ],
