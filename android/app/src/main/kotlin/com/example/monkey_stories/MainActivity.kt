155a06ff5d97001085e81fd884a4fab81b84a4e3
Quốc Trưởng KT <truong.dinh@hoc10.com>
2025-06-12 17:50:31 +0700

    refactor: thay đổi MainActivity từ FlutterActivity sang AudioServiceActivity để cải thiện khả năng phát nhạc

--- android/app/src/main/kotlin/com/example/monkey_stories/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/monkey_stories/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.monkey_stories
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 import co.ab180.airbridge.flutter.AirbridgeFlutter
 import android.content.Intent
 
-class MainActivity : FlutterActivity() {
+class MainActivity : AudioServiceActivity() {
     override fun onResume() {
         super.onResume()
         AirbridgeFlutter.trackDeeplink(intent)
