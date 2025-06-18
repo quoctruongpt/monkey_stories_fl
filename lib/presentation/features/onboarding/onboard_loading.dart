d2d7f5a5f6675542913608dd00eeaa5add49fb33
Quốc Trưởng KT <truong.dinh@hoc10.com>
2025-06-18 09:55:27 +0700

    feat: push event ms_ob_view_personalize_loading_screen

--- lib/presentation/features/onboarding/onboard_loading.dart
+++ lib/presentation/features/onboarding/onboard_loading.dart
@@ -4,9 +4,12 @@ import 'package:go_router/go_router.dart';
 import 'package:monkey_stories/core/constants/routes_constant.dart';
 import 'package:monkey_stories/core/localization/app_localizations.dart';
 import 'package:monkey_stories/core/theme/app_theme.dart';
+import 'package:monkey_stories/di/usecases.dart';
+import 'package:monkey_stories/domain/usecases/tracking/onboarding/ms_ob_view_personalize_loading_screen.dart';
 import 'package:monkey_stories/presentation/bloc/onboarding/onboarding_cubit.dart';
 import 'package:monkey_stories/presentation/widgets/base/notice_dialog.dart';
 import 'package:monkey_stories/presentation/widgets/create_profile_loading_view.dart';
+import 'package:monkey_stories/presentation/widgets/screen_tracker.dart';
 
 class OnboardLoading extends StatefulWidget {
   const OnboardLoading({super.key});
@@ -16,6 +19,8 @@ class OnboardLoading extends StatefulWidget {
 }
 
 class _OnboardLoadingState extends State<OnboardLoading> {
+  DateTime timeStart = DateTime.now();
+
   @override
   void initState() {
     super.initState();
@@ -23,6 +28,18 @@ class _OnboardLoadingState extends State<OnboardLoading> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<OnboardingCubit>().onStart();
     });
+  }
+
+  void _onTrackPush() {
+    timeStart = DateTime.now();
+  }
+
+  void _onTrackExit() {
+    sl<MsObViewPersonalizeLoadingScreenTrackingUsecase>().call(
+      MsObViewPersonalizeLoadingScreenParams(
+        timeOnScreen: DateTime.now().difference(timeStart).inSeconds,
+      ),
+    );
   }
 
   void _onError(OnboardingError error) {
@@ -51,28 +68,33 @@ class _OnboardLoadingState extends State<OnboardLoading> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<OnboardingCubit, OnboardingState>(
-      listenWhen: (previous, current) {
-        return previous.error != current.error && current.error != null;
-      },
-      listener: (context, state) {
-        _onError(state.error!);
-      },
+    return ScreenTracker(
+      routeName: AppRouteNames.onboardLoading,
+      onTrackPush: _onTrackPush,
+      onTrackExit: _onTrackExit,
       child: BlocListener<OnboardingCubit, OnboardingState>(
-        listenWhen:
-            (previous, current) => previous.progress != current.progress,
-        listener: (context, state) {
-          if (state.progress == 1) {
-            context.go(AppRoutePaths.obdPurchase);
-          }
+        listenWhen: (previous, current) {
+          return previous.error != current.error && current.error != null;
         },
-        child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: Spacing.lg),
-            child: BlocBuilder<OnboardingCubit, OnboardingState>(
-              builder: (context, state) {
-                return CreateProfileLoadingView(progress: state.progress);
-              },
+        listener: (context, state) {
+          _onError(state.error!);
+        },
+        child: BlocListener<OnboardingCubit, OnboardingState>(
+          listenWhen:
+              (previous, current) => previous.progress != current.progress,
+          listener: (context, state) {
+            if (state.progress == 1) {
+              context.go(AppRoutePaths.obdPurchase);
+            }
+          },
+          child: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: Spacing.lg),
+              child: BlocBuilder<OnboardingCubit, OnboardingState>(
+                builder: (context, state) {
+                  return CreateProfileLoadingView(progress: state.progress);
+                },
+              ),
             ),
           ),
         ),
